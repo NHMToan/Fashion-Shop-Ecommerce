3ae0d9c8d001003c489d25ca910022a8c501e465
Nguyễn Toàn <nguyentoan@MacBook-Air-cua-Nguyen.local>
2019-05-03 01:19:59 +0700

    add

--- billing/models.py
+++ billing/models.py
@@ -8,7 +8,8 @@ User = settings.AUTH_USER_MODEL
 
 
 import stripe
-stripe.api_key = "sk_test_ftmWBmegu5uTBT1BGN6GTy4t00NCATGFGO"
+STRIPE_SECRET_KEY = getattr(settings,"STRIPE_SECRET_KEY","sk_test_ftmWBmegu5uTBT1BGN6GTy4t00NCATGFGO")
+stripe.api_key = STRIPE_SECRET_KEY
 
 class BillingProfileManager(models.Manager):
 	def new_or_get(self, request):
