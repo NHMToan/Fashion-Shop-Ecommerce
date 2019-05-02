c60db32b2425b0c03a580e59d2a68a130f83733f
Nguyễn Toàn <nguyentoan@MacBook-Air-cua-Nguyen.local>
2019-05-03 01:02:12 +0700

    add

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY','0ypk!t!en_*79i-pvs0&#ni=f50r82eyrrw@l%7-)@0vt$b9=$')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1']
