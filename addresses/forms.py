f408358daeb3caa54fb19dd76c7d9a89b8c149bb
Nguyễn Toàn <nguyentoan@MacBook-Air-cua-Nguyen.local>
2019-05-08 16:32:43 +0700

    a

--- addresses/forms.py
+++ addresses/forms.py
@@ -16,3 +16,11 @@ class AddressForm(forms.ModelForm):
 			'state',
 			'postal_code'
 		]
+		widgets = {
+            'address_line_1': forms.TextInput(attrs={"class":'form-control', "placeholder":"Address line 1", "style":"border: 1px solid gray !important"}),
+            'address_line_2': forms.TextInput(attrs={"class":'form-control', "placeholder":"Address line 2", "style":"border: 1px solid gray !important"}),
+            'city': forms.TextInput(attrs={"class":'form-control', "placeholder":"city", "style":"border: 1px solid gray !important"}),
+            'country': forms.TextInput(attrs={"class":'form-control', "placeholder":"country", "style":"border: 1px solid gray !important"}),
+            'state': forms.TextInput(attrs={"class":'form-control', "placeholder":"state", "style":"border: 1px solid gray !important"}),
+            'postal_code': forms.TextInput(attrs={"class":'form-control', "placeholder":"Postal code", "style":"border: 1px solid gray !important"})
+        }
