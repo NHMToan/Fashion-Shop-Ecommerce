5cac80af357b3d495a7b0253338ef655fce819fc
Nguyễn Toàn <nguyentoan@MacBook-Air-cua-Nguyen.local>
2019-05-14 12:30:20 +0700

    a

--- products/models.py
+++ products/models.py
@@ -98,6 +98,7 @@ class Product(models.Model):
 	slug		= models.SlugField(blank=True,unique=True)
 	description = models.TextField()
 	price		= models.DecimalField(decimal_places=2,max_digits=20,default=39.99)
+	before_discount_price = models.DecimalField(decimal_places=2,max_digits=20,blank=True,null=True)
 	image		= models.ImageField(upload_to=upload_image_path,null=True,blank=True)
 	image2		= models.ImageField(upload_to=upload_image_path,null=True,blank=True)
 	image3		= models.ImageField(upload_to=upload_image_path,null=True,blank=True)
