61e62073b2dafcec89a2041007849672fd53ab2c
Nguyễn Toàn <nguyentoan@MacBook-Air-cua-Nguyen.local>
2019-05-23 00:56:57 +0700

    a

--- carts/models.py
+++ carts/models.py
@@ -47,6 +47,7 @@ class Cart(models.Model):
 
 
 
+
 def m2m_changed_cart_receiver(sender, instance,action, *args, **kwargs):
 	if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
 		products = instance.products.all()
